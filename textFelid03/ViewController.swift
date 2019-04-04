1a8aa1aa19e4a5bfe327d21e45d4ab7d77b20d70
D7703_07 <d7703_07@lulzsec.local>
2019-04-04 12:57:11 +0900

    ddddd

--- textFelid03/ViewController.swift
+++ textFelid03/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var lbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TextField.delegate = self as! UITextFieldDelegate
+        TextField.clearButtonMode = UITextField.ViewMode.always
     }
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let inString = TextField.text
+        print(inString!)
+        lbl.text = inString
+        TextField.text = ""
+        TextField.resignFirstResponder()
+    }
+    
+    public func textFieldShouldReturn(_textField: UITextField) -> Bool {
+        TextField.resignFirstResponder()
+        TextField.backgroundColor = UIColor.yellow
+        return true
+    }
+    
+    public func textFieldShouldClear(_textField: UITextField) -> Bool{
+        view.backgroundColor = UIColor.green
+        return true
+    }
+    
 
 }
 
